ae865e6edd056c1ff5a494adf7ebbbbb2321b9c3
Mihir Chaturvedi <plibither8@gmail.com>
2018-06-10 23:46:19 +0530

    Add trailing line-break character to outputs to cater for zsh users

--- src/write-license.js
+++ src/write-license.js
@@ -11,10 +11,10 @@ exports.writeLicense = (fullName, licenseName) => {
 
     fs.writeFile(resolve(process.cwd(), 'LICENSE'), text, (err) => {
         if (err) {
-            process.stdout.write(red.bold('\n❌ An error occured. Please try again.'));
+            process.stdout.write(red.bold('\n❌ An error occured. Please try again.\n'));
             return err;
         }
-        process.stdout.write(green.bold(`\n✔️ Successfully created LICENSE file with ${licenseName}`));
+        process.stdout.write(green.bold(`\n✔️ Successfully created LICENSE file with ${licenseName}\n`));
     });
 
 };
