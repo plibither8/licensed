9feaa30a3b30991669eebc57736228bba747a18d
Mihir Chaturvedi <plibither8@gmail.com>
2018-06-12 13:31:17 +0530

    Fix linting issues

--- src/generate-list.js
+++ src/generate-list.js
@@ -1,13 +1,13 @@
 const {licenses} = require('./licenses');
 const {pickLicense} = require('./choose-license/pick-license.js');
-const {green, bold} = require('chalk');
+const {green} = require('chalk');
 
 exports.generateList = () => {
 
     process.stdout.write('\n');
 
     pickLicense().then(name => {
-        process.stdout.write(bold.green(`\n    ${name}:\n`))
+        process.stdout.write(green.bold(`\n    ${name}:\n`));
         process.stdout.write(`\n${licenses[name].value}\n`);
     });
 
