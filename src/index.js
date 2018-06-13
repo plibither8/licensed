51e79b4ce2d15d580239a373a7421643a72b4c9d
Mihir Chaturvedi <plibither8@gmail.com>
2018-06-13 17:02:16 +0530

    Implement confirmation prompt if LICENSE file exists in directory

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ const meow = require('meow');
 const {chooseLicense} = require('./choose-license');
 const {searchLicense} = require('./search-license');
 const {generateList} = require('./generate-list');
+const {
+    licenseFileExists,
+    licenseExistsPrompt,
+    viewExistingLicense,
+    licenseExistsExit,
+    licenseExistsPromptChoices
+} = require('./license-exists.js');
 
 const cli = meow(`Usage:
     $ licensed                  # Brings up an option to start a questionnaire or choose from a list of available licenses
@@ -43,10 +50,34 @@ Copyright (c) 2018, Mihir Chaturvedi`,
     },
 });
 
+const start = (userInput, flags) => {
+
+    /**
+     * If called without inputs,
+     * get a list of available inputs and prompt the
+     * user to choose some stuff.
+     */
+
+    if (!userInput.length) {
+        chooseLicense(flags);
+    }
+
+    /**
+     * If called with an input
+     * fuzzy search amongst license names
+     * and return the first result
+     */
+
+    else {
+        searchLicense({input: userInput}, flags);
+    }
+
+};
+
 /**
  * If licensed is called with
  * `-l` or `--list` flag, generate
- * a list of available licenses to 
+ * a list of available licenses to
  * choose from
  */
 
@@ -57,23 +88,36 @@ if (cli.flags.l) {
 else {
 
     /**
-     * If called without inputs,
-     * get a list of available inputs and prompt the
-     * user to choose some stuff.
+     * If a LICENSE file already exists
+     * ask user whether to continue with
+     * process or abort
      */
 
-    if (!cli.input.length) {
-        chooseLicense(cli.flags);
+    if (licenseFileExists()) {
+
+        licenseExistsPrompt().then(response => {
+
+            /* If user decides to abort */
+            if (response === licenseExistsPromptChoices[0]) {
+                licenseExistsExit();
+                return;
+            }
+
+            /* If user decides to view existing license */
+            if (response === licenseExistsPromptChoices[1]) {
+                viewExistingLicense();
+                return;
+            }
+
+            /* If user decides to continue with creating a new license */
+            start(cli.input, cli.flags);
+
+        });
+
     }
 
-    /**
-     * If called with an input
-     * fuzzy search amongst license names
-     * and return the first result
-     */
-
     else {
-        searchLicense({input: cli.input}, cli.flags);
+        start(cli.input, cli.flags);
     }
 
 }
---
+++ src/license-exists.js
@@ -0,0 +1,46 @@
+const {resolve} = require('path');
+const {existsSync} = require('fs');
+const {prompt} = require('inquirer');
+const {red} = require('chalk');
+const {readFileSync} = require('fs');
+const {underline} = require('chalk');
+
+const choices = [
+    'Do not create a new LICENSE file',
+    'View existing LICENSE file',
+    'Overwrite current LICENSE file and create a new one'
+]
+
+exports.licenseFileExists = () => {
+    return existsSync(resolve(process.cwd(), 'LICENSE'));
+};
+
+exports.licenseExistsPrompt = async () => {
+    process.stdout.write('\n');
+
+    const {licenseExistsResponse} = await prompt([
+        {
+            type: 'rawlist',
+            name: 'licenseExistsResponse',
+            message: 'A LICENSE file already exists in this directory. Please choose how to proceed:',
+            choices: choices,
+            highlight: true,
+            searchable: true,
+        }
+    ]);
+
+    return licenseExistsResponse;
+};
+
+exports.viewExistingLicense = () => {
+
+    const licenseText = readFileSync(resolve(process.cwd(), 'LICENSE'));
+    process.stdout.write(`\n    ${underline('Existing LICENSE file:')}\n\n${licenseText}\n`);
+
+};
+
+exports.licenseExistsExit = () => {
+    process.stdout.write(red.bold('\n❌ LICENSE file not created.\n'));
+}
+
+exports.licenseExistsPromptChoices = choices;
